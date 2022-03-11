4a88fab04bbaeefc230407cd5b051ca43e8f9df7
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2022-03-11 23:10:03 +0300

    туц

---
+++ Scripts to Scrolls/Скроллить при нажатии/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $("#Menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+ 
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+ 
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+         
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
+/*______________________________________ButtonTop___________________________________________*/
+
+$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
+
+/* Прокрутка по нажатию на кнопку*/
+$(document).ready(function(){
+    $('#KontentblockSilka').click(function(){
+       var target = $(this).attr('href');
+       $('html, body').animate({scrollTop: $(target).offset().top}, 1000);
+       return false;
+    });
+ });
+/*______________________________________С подключением Jqery в HTML_____________________________*/
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
