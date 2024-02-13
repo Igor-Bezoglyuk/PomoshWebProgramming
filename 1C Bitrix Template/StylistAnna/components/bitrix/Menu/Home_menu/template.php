0100d982051da07189bf2ffef96a3f071d8b756a
Igor-Bezoglyuk <igor.bezoglyuk@mail.ru>
2024-02-13 22:02:19 +0300

    Add files via upload

--- 1C Bitrix Template/StylistAnna/components/bitrix/Menu/Home_menu/template.php
+++ 1C Bitrix Template/StylistAnna/components/bitrix/Menu/Home_menu/template.php
@@ -1,1 +1,11 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
+
+<?php if (!empty($arResult)): ?><!--Выводит список страниц который ты создал не забудь подключить  код вывода в HTML-->
+    
+<ul class="Header-Menu">
+<?php foreach ($arResult as $item): ?>
+    <li class="Menu-li"><a class="Menu-link" href="<?= $item['LINK'] ?>"><?= $item['TEXT'] ?></a></li>
+	<?php endforeach; ?>
+</ul
+<?php endif; ?>
