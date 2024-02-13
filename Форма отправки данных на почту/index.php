789bf79c45261c8aa533665d325a5bc706b76a2f
Igor-Bezoglyuk <igor.bezoglyuk@mail.ru>
2024-02-13 22:35:51 +0300

    folder

---
+++ Форма отправки данных на почту/index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<header>
+	<div id="headerBlock">
+	<h1 id="headerBlockText">Подьём Стройфирма</h1>
+	<div id="headerBlock2Img"></div>
+	</div>
+
+	<ul id="Menu">
+		<li><a href="">Главная</a></li>
+		<li><a href="">О нас</a></li>
+		<li><a href="">Услуги</a></li>
+		<li><a href="">Товары</a></li>
+		<li><a href="">Контакты</a></li>
+	</ul>
+</header>
+
+<article>
+
+	<section id="Kontent">
+	  <div id="KontentBlock">
+	  	<h1 id="KontentBlockText">ЛИДЕРЫ В ОБЛАСТИ КАЧЕСТВЕННОГО СТРОИТЕЛЬСТВА</h1>
+	  </div>
+	</section>
+
+	<section id="Kontent2">
+	 <h1 id="Kontent2Text">УСЛУГИ</h1>
+
+	 <div id="Kontent2Block">
+	 	<div id="Kontent2BlockImg"></div>
+	 	<h1 id="Kontent2BlockText">Планирование строительства</h1>
+	 	<p id="Kontent2BlockStroka">Это текст. Нажмите один раз и выберите «Редактировать текст» или просто дважды кликните, чтобы добавить свой текст и настроить шрифт.</p>
+	 </div>
+
+	 <div id="Kontent2Block">
+	 	<div id="Kontent2BlockImg2"></div>
+	 	<h1 id="Kontent2BlockText">Архитектурное моделирование</h1>
+	 	<p id="Kontent2BlockStroka">Это текст. Нажмите один раз и выберите «Редактировать текст» или просто дважды кликните, чтобы добавить свой текст и настроить шрифт.</p>
+	 </div>
+
+	 <div id="Kontent2Block">
+	 	<div id="Kontent2BlockImg3"></div>
+	 	<h1 id="Kontent2BlockText">Управление строительством</h1>
+	 	<p id="Kontent2BlockStroka">Это текст. Нажмите один раз и выберите «Редактировать текст» или просто дважды кликните, чтобы добавить свой текст и настроить шрифт.</p>
+	 </div>
+	</section>
+
+	<section id="Kontent3">
+		<h1 id="Kontent3Text">О НАС</h1>
+		<p id="Kontent3Stroka">Это текст. Нажмите один раз и выберите «Редактировать текст» или просто кликните дважды, чтобы добавить свой текст и настроить шрифт. Вы можете переместить его в любое место на странице. Расскажите посетителям сайта о себе. Здесь будет удачно смотреться текст о вашей компании и услугах. Используйте эту возможность, чтобы выгодно представить себя и свою компанию клиентам. Расскажите интересную историю, например, как вам в голову пришла идея собственного дела, и объясните, в чем заключается ваше преимущество перед конкурентами.</p>
+		<div id="Kontent3Img"></div>
+	</section>
+    
+    <section id="Kontent4">
+
+    	<div id="Kontent4Block">
+    		<h1 id="Kontent4BlockText">2023</h1>
+    		<p id="Kontent4BlockStroka">Год основания</p>
+    	</div>
+
+    	<div id="Kontent4Block">
+    		<h1 id="Kontent4BlockText">206</h1>
+    		<p id="Kontent4BlockStroka">Завершенных проектов</p>
+    	</div>
+
+    	<div id="Kontent4Block">
+    		<h1 id="Kontent4BlockText">870</h1>
+    		<p id="Kontent4BlockStroka">Строительных подрядов</p>
+    	</div>
+
+    	<div id="Kontent4Block">
+    		<h1 id="Kontent4BlockText">26</h1>
+    		<p id="Kontent4BlockStroka">Полученных наград</p>
+    	</div>
+
+    </section>
+
+    <section id="Kontent5">
+
+    	<h1 id="Kontent5Text">ПРОЕКТЫ</h1>
+
+    	<div id="Kontent5Block">
+    		<div id="Kontent5BlockIMG"></div>
+    		<div id="Kontent5BlockSilka"></div>
+    	</div>
+
+    	<div id="Kontent5Block">
+    		<div id="Kontent5BlockIMG2"></div>
+    		<div id="Kontent5BlockSilka"></div>
+    	</div>
+
+    	<div id="Kontent5Block">
+    		<div id="Kontent5BlockIMG3"></div>
+    		<div id="Kontent5BlockSilka"></div>
+    	</div>
+
+    	<div id="Kontent5Block">
+    		<div id="Kontent5BlockIMG4"></div>
+    		<div id="Kontent5BlockSilka"></div>
+    	</div>
+
+    </section>
+
+    <section id="Kontent6">
+    	<h1 id="Kontent6Text">КЛИЕНТЫ</h1>
+
+    	<div id="Kontent6IMG"></div>
+    	<div id="Kontent6IMG2"></div>
+    	<div id="Kontent6IMG3"></div>
+    	<div id="Kontent6IMG4"></div>
+
+    </section>
+
+    <section id="Kontent7">
+    	<h1 id="Kontent7Text">КОНТАКТЫ</h1>
+
+    	<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A1a9d9fa1c3b7f0807833ea9393f12808f552552c09e8b1d67b99e07163e58848&amp;source=constructor"  frameborder="0"></iframe>
+    </section>
+</article>
+
+<footer>
+ <h1 id="footerText">Ответы на вопросы</h1>
+ <p id="footerStroka">С удовольствем ответим на все ваши вопросы по телефону +7 (495) 000-00-00. Или просто заполните форму ниже:</p>
+
+ <form id="footerForma" name="Forma" action="Mail.php" method="POST">
+ 	<input id="formText" type="Text" name="name" placeholder="Name">
+ 	<input id="formText" type="Text" name="emil" placeholder="Email">
+ 	<input id="formText" type="Text" name="phone" placeholder="Subject">
+ 	<textarea id="formTextarea" type="Texterea" name="massage" placeholder="Massage"></textarea>
+    <input id="formKnopka" type="submit" name="Knopka" value="Отправить">
+ </form>
+
+ <div id="footerBlock">
+ 	<h1 id="footerBlockText">Центральный офис</h1>
+ 	<p id="footerBlockStroka">ул. Арбат, 1а, Москва 119019, Россия</p>
+ 	<p id="footerBlockStroka">info@mysite.ru , Телефон: +7 (495) 000-00-00</p>
+ </div>	
+
+ <a id="footerSilka" href=""></a>
+ <a id="footerSilka2" href=""></a>
+ <a id="footerSilka3" href=""></a>
+ 
+</footer>
+</body>
+</html>
