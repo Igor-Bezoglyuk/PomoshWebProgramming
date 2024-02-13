00dcf0ae883cb4e83e82c83e26065d23e6e657cc
Igor-Bezoglyuk <igor.bezoglyuk@mail.ru>
2024-02-13 22:35:29 +0300

    php

--- Форма отправки данных в БД/Baze.php
+++ Форма отправки данных в БД/Baze.php
@@ -1,1 +1,32 @@
+<?php
+if (isset($_POST['name']) && isset($_POST['EMail'])){
 
+    // Переменные с формы
+    $name = $_POST['name'];
+    $email = $_POST['EMail'];
+    
+    // Параметры для подключения
+    $host = "localhost"; 
+    $user = "root"; // Логин БД
+    $password = ""; // Пароль БД
+    $base = 'MyBazaDanih'; // Имя БД
+    $table = "people"; // Имя Таблицы БД
+    
+    // Подключение к базе данных
+    $mysqli = @new mysqli($host,$user,$password,$base);
+
+    // Если есть ошибка соединения, выводим её и убиваем подключение
+	if ($mysqli->connect_error) {
+	    die('Ошибка : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+	}
+     header("Location: index.php"); // Направляет пользователя после отправки даных в БД, адресс вводите сами.
+
+    $result = $mysqli->query("INSERT INTO ".$table." (name,EMail) VALUES ('$name','$email')");
+    
+    if ($result == true){
+    	echo "Информация занесена в базу данных";
+    }else{
+    	echo "Информация не занесена в базу данных";
+    }
+}
+?>
---
+++ Форма отправки данных в БД/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+ <title>Запись в БД через форму на php</title>
+</head>
+<body>
+ <form method="POST" action="Baze.php">
+  <input name="name" type="text" placeholder="Имя"/>
+  <input name="EMail" type="text" placeholder="Email"/>
+  <input type="submit" value="Отправить"/>
+ </form>
+</body>
+</html>
